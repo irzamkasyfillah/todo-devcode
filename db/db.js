791b3623c1c30746170c205eb21178148ce81df6
irzamkasyfillah <irzamkasyfillah@gmail.com>
2022-09-25 00:42:25 +0800

    update

--- db/db.js
+++ db/db.js
@@ -3,10 +3,14 @@ require("dotenv").config()
 
 
 const connection = mysql.createConnection({
-    host: process.env.MYSQL_HOST,
-    user: process.env.MYSQL_USER,
-    password: process.env.MYSQL_PASSWORD,
-    database: process.env.MYSQL_DBNAME
+    // host: process.env.MYSQL_HOST || "localhost",
+    // user: process.env.MYSQL_USER || "root",
+    // password: process.env.MYSQL_PASSWORD || "",
+    // database: process.env.MYSQL_DBNAME || "todo-devcode"
+    host: "127.0.0.1",
+    user: "root",
+    password: "",
+    database: "todo-devcode"
 })
 
 connection.connect((err) => {
