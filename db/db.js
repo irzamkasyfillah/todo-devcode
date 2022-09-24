a79b07fc4c86ee6dc24aafca63e853f3c1c0dbf3
irzamkasyfillah <irzamkasyfillah@gmail.com>
2022-09-24 23:35:33 +0800

    change env variable name

--- db/db.js
+++ db/db.js
@@ -3,10 +3,10 @@ require("dotenv").config()
 
 
 const connection = mysql.createConnection({
-    host: process.env.DB_HOST,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASS,
-    database: process.env.DB_DATABASE
+    host: process.env.MYSQL_HOST,
+    user: process.env.MYSQL_USER,
+    password: process.env.MYSQL_PASSWORD,
+    database: process.env.MYSQL_DBNAME
 })
 
 connection.connect((err) => {
